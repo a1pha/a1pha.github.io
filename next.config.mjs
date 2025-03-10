d9953822236ec485dc8829a78a9a20a6e8e629a8
Abhi Jadhav <abhijadhav9571@gmail.com>
2025-03-09 21:45:21 -0700

    try to fix

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,13 @@ import createMDX from '@next/mdx';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx']
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  output: 'export',
+  distDir: 'dist',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/a1pha.github.io',
 };
 
 const withMDX = createMDX({
